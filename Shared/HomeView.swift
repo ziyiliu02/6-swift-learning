a48fa9b99740d668459bc9ee2a8553412a2d5681
Liu Ziyi <ziyi.liu02@gmail.com>
2023-05-27 13:44:15 +0800

    Initial Commit

---
+++ Shared/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  6-swift-learning
+//
+//  Created by Liu Ziyi on 27/5/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ Shared/__swift_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  __swift_learningApp.swift
+//  Shared
+//
+//  Created by Liu Ziyi on 27/5/23.
+//
+
+import SwiftUI
+
+@main
+struct __swift_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
