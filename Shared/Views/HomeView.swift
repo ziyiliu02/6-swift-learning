ac1fc421bbe992fc97b27a90726ceee6c1ac6a12
Liu Ziyi <ziyi.liu02@gmail.com>
2023-05-27 17:04:38 +0800

    - Create learning card in HomeView (parse the json data and display the list of content in the home view)

--- Shared/Views/HomeView.swift
+++ Shared/Views/HomeView.swift
@@ -12,12 +12,82 @@ struct HomeView: View {
     @EnvironmentObject var model: ContentModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView {
+            
+            LazyVStack {
+                
+                ForEach(model.modules) { module in
+                    
+                    // Learning Card
+                    ZStack {
+                        
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                            .aspectRatio(CGSize(width: 335, height: 175),contentMode: .fit)
+                        
+                        HStack {
+                            // Image
+                            Image(module.content.image)
+                                .resizable()
+                                .frame(width: 116, height: 116)
+                                .clipShape(Circle())
+                            
+                            Spacer()
+                            
+                            //Text
+                            VStack(alignment:.leading, spacing:10) {
+                                //Headline
+                                Text("Learn \(module.category)")
+                                    .bold()
+                                
+                                //Description
+                                Text(module.content.description)
+                                    .padding(.bottom, 20)
+                                    .font(.caption)
+                                
+                                //Icons
+                                HStack {
+                                    //Number of lessons/questions
+                                    Image(systemName: "text.book.closed")
+                                        .resizable()
+                                        .frame(width:15, height: 15)
+                                    Text("\(module.content.lessons.count) Lessons")
+                                        .font(.caption)
+                                    
+                                    Spacer()
+                                    
+                                    //Time
+                                    Image(systemName: "clock")
+                                        .resizable()
+                                        .frame(width:15, height: 15)
+                                    Text(module.content.time)
+                                        .font(.caption)
+                                }
+                            }
+                        }
+                            .padding(.horizontal, 20)
+                        
+                    }
+                    
+                    // Test Card
+                    
+                }
+                
+            }
+                .padding()
+            
+        }
+        
+        
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(ContentModel())
     }
 }
