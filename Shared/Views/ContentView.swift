678655ebf4cd2c2bc59aa82505519aa96faa6764
Liu Ziyi <ziyi.liu02@gmail.com>
2023-05-28 15:34:12 +0800

    - Abstract each lesson in ContentView to its own subview, ContentViewRow

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -21,28 +21,8 @@ struct ContentView: View {
                 if model.currentModule != nil {
                     ForEach(0..<model.currentModule!.content.lessons.count){ index in
                         
-                        let lesson = model.currentModule!.content.lessons[index]
+                        ContentViewRow(index: index)
                         
-                        // Lesson Card
-                        ZStack(alignment:.leading){
-                            Rectangle()
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                                .shadow(radius: 5)
-                                .frame(height: 66)
-                            
-                            HStack(spacing:30){
-                                Text(String(index + 1))
-                                    .bold()
-                                VStack(alignment:.leading){
-                                    Text(lesson.title)
-                                        .bold()
-                                    Text(lesson.duration)
-                                }
-                            }
-                            .padding()
-                        }
-                        .padding(.bottom, 10)
                     }
                 }
                 
